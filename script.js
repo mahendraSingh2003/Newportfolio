68cf25a6ca912e318424a99d824ace5277bae7c5
Mahendra Singh Gurjar <157353148+mahendraSingh2003@users.noreply.github.com>
2024-07-07 18:50:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/60df10bf7f4b000ac03ab6a8/1f9jlirg6';
+    s1.src = 'https://embed.tawk.to/668961b8eaf3bd8d4d18db0d/1i24abedq';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
